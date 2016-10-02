9cfb8e434b56bd024f3a12645536eac28b974e91
namilar <n.radith@gmail.com>
2016-10-02 15:31:05 +0530

    mid work commit

--- app/scripts/directives/cashier/header.js
+++ app/scripts/directives/cashier/header.js
@@ -9,10 +9,10 @@
 angular.module('saHackathon2016ClientApp')
   .directive('cashier/header', function () {
     return {
-      template: '<div></div>',
-      restrict: 'E',
+      template: 'views/cashier/header.html',
+      restrict: 'A',
       link: function postLink(scope, element, attrs) {
-        element.text('this is the cashier/header directive');
+
       }
     };
   });
