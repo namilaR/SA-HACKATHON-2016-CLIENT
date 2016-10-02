942c6e87312e5d701d061fb310ca1af1d6104cbe
DeAlwis <kasun.kulathunge@gmail.com>
2016-10-02 13:28:23 +0530

    -

---
+++ app/scripts/controllers/cashier/food-list.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name saHackathon2016ClientApp.controller:CustomerFoodListCtrl
+ * @description
+ * # CustomerFoodListCtrl
+ * Controller of the saHackathon2016ClientApp
+ */
+angular.module('saHackathon2016ClientApp')
+  .controller('CustomerFoodListCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/cashier/header.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name saHackathon2016ClientApp.controller:CustomerHeaderCtrl
+ * @description
+ * # CustomerHeaderCtrl
+ * Controller of the saHackathon2016ClientApp
+ */
+angular.module('saHackathon2016ClientApp')
+  .controller('CustomerHeaderCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
